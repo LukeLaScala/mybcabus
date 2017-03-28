e4d11bc37fc9e3b00e67d2dccdeb9719476f764b
Luke LaScala <lascala.luke@gmail.com>
2017-03-28 18:02:00 -0400

    Fully functional live updating

--- bcabus.js
+++ bcabus.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $favoriteList = $(".favoriteList");
-    
-    //Display message if cookie does not exist
+    $madeBy = $(".madeBy");
+    $closeMsg = $(".closeMadeBy");
+
     function getCookie(name) {
         var dc = document.cookie;
         var prefix = name + "=";
         var begin = dc.indexOf("; " + prefix);
         if (begin == -1) {
             begin = dc.indexOf(prefix);
-            if (begin != 0) return null;
+            if (begin !== 0) return null;
         }
         else
         {
@@ -20,30 +21,12 @@ $(document).ready(function() {
         }
         return decodeURI(dc.substring(begin + prefix.length, end));
     }
-        var myCookie = getCookie("favorite");
-        if (myCookie == null) {
-            Materialize.toast("Click the star to set a favorite town",6000);
-        }
-        else {
-            $favoriteList.show(100);
-        }
-    
-    //Works by #id of the list element
-    $search = $("input[type='search']");
-    $townItem = $(".townItem");
-    $townHeader = $(".townList-header");
-    $search.keyup(function() {
-        $town = $search.val().toLowerCase();
-        if ($town !== "") {
-            $townItem.not("[id*='" + $town + "']").hide(100);
-            $(".collection-item[id*='" + $town + "']").show(100);
-            $townHeader.hide(100);
-            $favoriteList.hide(100);
-        }
-        else {
-            $townItem.show(100);
-            $townHeader.show(100);
-            if (localStorage.getItem("favTown") !== null) { $favoriteList.show(100); };
-        }
+    var favCookie = getCookie("favorite");
+    if (favCookie === null) {
+        Materialize.toast("Click the star to set a favorite town",6000);
+    }
+    var closeCookie = getCookie("madeby");
+    $closeMsg.click(function() {
+        $madeBy.slideUp(100);
     })
 })
